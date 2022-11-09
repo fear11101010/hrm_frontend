7dee13372f0df449259ef2654a993d634b36f1cd
Arafat <shuvo11101010@gmail.com>
2022-11-09 18:49:09 +0600

    test

--- src/layout/navbar/navbar1.js
+++ src/layout/navbar/navbar1.js
@@ -6,8 +6,18 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Collapse} from "react-bootstrap";
+import {useState} from "react";
 
 function Navbar1(props){
+    const [menuOpenCloseState,setMenuOpenCloseState] = useState([false])
+    const openOrCloseMenu = (i)=>{
+        // console.log(e);
+        const menus = [...menuOpenCloseState];
+        const currentMenuState = menuOpenCloseState[i];
+        menus.fill(false);
+        menus[i] = !currentMenuState
+        setMenuOpenCloseState(menus);
+    }
     return (
         <Navbar expand="md" fixed="top" variant="light" className="navbar-vertical">
             <Container fluid>
@@ -26,27 +36,29 @@ function Navbar1(props){
                     </Form>
                     <Nav as="ul">
                         <Nav.Item as={"li"}>
-                            <Nav.Link href="#"  data-bs-toggle="collapse" role="button" aria-expanded="true" aria-controls="sidebarDashboards">
+                            <Nav.Link onClick={()=>openOrCloseMenu(0)} href="#" className={menuOpenCloseState[0]?'collapsed':''}  data-bs-toggle="collapse" role="button" aria-expanded={menuOpenCloseState[0]?"true":"false"} aria-controls="sidebarDashboards">
                                 <i className="fe fe-home"></i> Dashboards
                             </Nav.Link>
-                            <Collapse in={true}>
-                                <ul className="nav nav-sm flex-column">
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link active">
-                                            Default
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link ">
-                                            Project Management
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link ">
-                                            E-Commerce
-                                        </a>
-                                    </li>
-                                </ul>
+                            <Collapse in={menuOpenCloseState[0]}>
+                                <div id="sidebarDashboards">
+                                    <ul  className="nav nav-sm flex-column">
+                                        <li className="nav-item">
+                                            <a href="#" className="nav-link active">
+                                                Default
+                                            </a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="#" className="nav-link ">
+                                                Project Management
+                                            </a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="#" className="nav-link ">
+                                                E-Commerce
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </Collapse>
                         </Nav.Item>
                     </Nav>
