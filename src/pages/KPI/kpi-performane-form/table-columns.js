e5cd9dc1a59d63f41050708d161847586490ef3b
sarder safa bin salam <sarder.safa@datasoft-bd.com>
2022-11-21 17:06:53 +0600

    ICON fixed

--- src/pages/KPI/kpi-performane-form/table-columns.js
+++ src/pages/KPI/kpi-performane-form/table-columns.js
@@ -1,44 +1,45 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
-    EMPLOYEE_ASSESTMENT_SINGLE_PAGE,
-    EMPLOYEE_PERFORMANCE_VIEW,
-    KPI_PERMORMANCE_FORM_PAGE
+  EMPLOYEE_ASSESTMENT_SINGLE_PAGE,
+  EMPLOYEE_PERFORMANCE_VIEW,
+  KPI_PERMORMANCE_FORM_PAGE,
 } from "../../../utils/APP_ROUTES";
 import React from "react";
 
 export const kpiPerformanceFormColumns = [
-    {
-        name:'#',
-        selector:(row,index)=>index+1
-    },
-    {
-        name:'Year',
-        selector:row=>row.year
-    },
-    {
-        name:'ID',
-        cell:row=>(
-            <Link to={EMPLOYEE_PERFORMANCE_VIEW(row.id)}>{row.employee.employee_id}</Link>
-        )
-    },
-    {
-        name:'Name',
-        selector:row=>row.employee.name
-    },
-    {
-        name:'Designation',
-        selector:row=>row.employee.designation
-    },
-    {
-        name: "Action",
-        cell: (row) => (
-                <div className="d-flex justify-content-center align-items-center w-100">
-                    <Link className={`btn btn-rounded-circle btn-sm btn-primary ${row.flag?"disabled":""}`} to={KPI_PERMORMANCE_FORM_PAGE(row.id)}>
-                        <i className="fe fe-edit"></i>
-                    </Link>
-                </div>
-        ),
-        width:'80px',
-        wrap: true,
-    },
-]
+  {
+    name: "#",
+    selector: (row, index) => index + 1,
+  },
+  {
+    name: "Year",
+    selector: (row) => row.year,
+  },
+  {
+    name: "ID",
+    cell: (row) => <Link to={EMPLOYEE_PERFORMANCE_VIEW(row.id)}>{row.employee.employee_id}</Link>,
+  },
+  {
+    name: "Name",
+    selector: (row) => row.employee.name,
+  },
+  {
+    name: "Designation",
+    selector: (row) => row.employee.designation,
+  },
+  {
+    name: "Action",
+    cell: (row) => (
+      <div className="d-flex justify-content-center align-items-center w-100">
+        <Link
+          className={`btn btn-rounded-circle btn-sm btn-primary ${row.flag ? "disabled" : ""}`}
+          to={KPI_PERMORMANCE_FORM_PAGE(row.id)}
+        >
+          <i className="fe fe-file-text"></i>
+        </Link>
+      </div>
+    ),
+    width: "80px",
+    wrap: true,
+  },
+];
