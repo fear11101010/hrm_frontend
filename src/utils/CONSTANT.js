fc626ffed5cb4333b45bf0ae61bc24b3ee9e0b57
Arafat <shuvo11101010@gmail.com>
2022-12-18 18:11:06 +0600

    media_url fix

--- src/utils/CONSTANT.js
+++ src/utils/CONSTANT.js
@@ -7,7 +7,8 @@ const server_ip = "http://192.168.7.148:9998";
 export const BASE_URL = "http://127.0.0.1:8000/api";
 // export const BASE_URL = `${server_ip}/api`;
 
-export const BASE_URL_FOR_MEDIA_FILE = "http://127.0.0.1:8000/media/";
+// export const BASE_URL_FOR_MEDIA_FILE = "http://127.0.0.1:8000/media/";
+export const BASE_URL_FOR_MEDIA_FILE = "http://182.163.112.207:9998/media/";
 export const DATE_FORMAT = " DD-MMM-YYYY";
 export const ASSESTMENT_TYPE = [
   { label: "Yearly", value: "Yearly" },
