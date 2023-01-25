ce96c298d22982b6aaffd7e3ef45ac881ac82b9e
sarder safa bin salam <sarder.safa@datasoft-bd.com>
2023-01-25 17:48:39 +0600

    Landing page changed

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -12,16 +12,16 @@ export default function Landing() {
   return token ? (
     <InnerLanding />
   ) : (
-    <div className="landing">
+    <div className="landing" style={{ backgroundColor: "#fff" }}>
       <div>
         <NavbarPublic />
         <Container>
           <Row className="content" style={{ height: "85vh" }}>
             <Col sm="12" md={{ span: 6, order: 0 }} xs={{ order: 1 }}>
-              <h1 className="mb-3">DS Ticketing System</h1>
+              <h1 className="mb-3"> DataSoft HR & Admin Ticketing System</h1>
               <h4 className="mb-4" style={{ fontWeight: 400 }}>
-                DataSoft Ticket System is the single best ticket System for platform. With the help of this module customer
-                can generate ticket to Admin and with help of staff admin can manage them and himself too.
+                DataSoft HR & Admin Ticketing System is a robust platform for tackling internal admin and HR issues. Through
+                this system, users can generate tickets on Admin and HR issues and receive prompt service
               </h4>
             </Col>
             <Col sm="12" md={{ span: 6, order: 0 }} xs={{ order: 0 }} className="text-end">
@@ -35,21 +35,26 @@ export default function Landing() {
 }
 
 {
-  /* <Container>
-<Row className="content">
-  <Col sm="12" md={{ span: 5, order: 0 }} xs={{ order: 1 }}>
-    <h1 className="mb-3">
-      Performance Appraisals <br /> For Human Resources
-    </h1>
-    <h4 className="mb-4" style={{ fontWeight: 400 }}>
-      Evaluate employee performance by combining a multitude of employee performance parameters, whether
-      quantitative, qualitative or both, in order to optimize your performance management process while reducing
-      rater bias.
-    </h4>
-  </Col>
-  <Col sm="12" md={{ span: 7, order: 0 }} xs={{ order: 0 }}>
-    <Lottie animationData={landingAnimation} style={{ width: "90%", margin: "auto" }} />
-  </Col>
-</Row>
-</Container> */
+  // <div className="landing">
+  //   <div>
+  //     <NavbarPublic />
+  //     <Container>
+  //       <Row className="content">
+  //         <Col sm="12" md={{ span: 5, order: 0 }} xs={{ order: 1 }}>
+  //           <h1 className="mb-3">
+  //             Performance Appraisals <br /> For Human Resources
+  //           </h1>
+  //           <h4 className="mb-4" style={{ fontWeight: 400 }}>
+  //             Evaluate employee performance by combining a multitude of employee performance parameters, whether
+  //             quantitative, qualitative or both, in order to optimize your performance management process while reducing
+  //             rater bias.
+  //           </h4>
+  //         </Col>
+  //         <Col sm="12" md={{ span: 7, order: 0 }} xs={{ order: 0 }}>
+  //           <Lottie animationData={landingAnimation} style={{ width: "90%", margin: "auto" }} />
+  //         </Col>
+  //       </Row>
+  //     </Container>
+  //   </div>
+  // </div>;
 }
