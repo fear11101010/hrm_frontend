cb62623be976f9f8c1c2dc36a96509ecd5d422e8
sarder safa bin salam <sarder.safa@datasoft-bd.com>
2023-01-26 10:20:13 +0600

    Performance Review---year summary bug fix

--- src/pages/hrm/KPI/kpi-performer-assestment/summary.js
+++ src/pages/hrm/KPI/kpi-performer-assestment/summary.js
@@ -21,25 +21,41 @@ export default function Summary({ rowId }) {
     {
       name: "Basic Salary",
       selector: (row) => row?.basic_salary,
-      cell: (row) => _Decode(row?.basic_salary?.toLocaleString("en-IN")),
+      cell: (row) => (
+        <>{_Decode(row?.basic_salary) === "None" ? "0" : _Decode(row?.basic_salary?.toLocaleString("en-IN"))}</>
+      ),
       right: true,
     },
     {
       name: "Gross Salary",
       selector: (row) => row?.gross_salary,
-      cell: (row) => _Decode(row?.new_gross_salary_b?.toLocaleString("en-IN")),
+      cell: (row) => (
+        <>{_Decode(row?.new_gross_salary_b) === "None" ? "0" : _Decode(row?.new_gross_salary_b?.toLocaleString("en-IN"))}</>
+      ),
       right: true,
     },
     {
       name: "Increment Amount",
       selector: (row) => row?.proposed_by_sbu_director_pm_self,
-      cell: (row) => _Decode(row?.proposed_by_sbu_director_pm_self?.toLocaleString("en-IN")),
+      cell: (row) => (
+        <>
+          {_Decode(row?.proposed_by_sbu_director_pm_self) === "None"
+            ? "0"
+            : _Decode(row?.proposed_by_sbu_director_pm_self?.toLocaleString("en-IN"))}
+        </>
+      ),
       right: true,
     },
     {
       name: <div className="text-end">Proposed Amount by Director</div>,
       selector: (row) => row?.proposed_by_sbu_director_pm_self,
-      cell: (row) => _Decode(row?.proposed_by_sbu_director_pm_self?.toLocaleString("en-IN")),
+      cell: (row) => (
+        <>
+          {_Decode(row?.proposed_by_sbu_director_pm_self) === "None"
+            ? "0"
+            : _Decode(row?.proposed_by_sbu_director_pm_self?.toLocaleString("en-IN"))}
+        </>
+      ),
       right: true,
     },
     {
